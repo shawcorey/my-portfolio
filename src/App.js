5742b60ea3b97928efde749da0ab6693a73edeb4
Corey L Shaw <shaw.corey@gmail.com>
2024-09-24 14:04:05 -0500

    Created routes for navigation
    Navigational routes started for site.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 import About from './components/About'; 
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Blog from './components/Blog';
+import Navbar from './components/Navbar'; // Optional navigation bar
+
+
 
 
 function App() {
   return (
     <div className="App">
+        <Router>
+            <div className="App">
+                {/* Optional: Navigation bar */}
+                <Navbar />
+
+                {/* Define Routes */}
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/blog" element={<Blog />} />
+                </Routes>
+            </div>
+        </Router> 
+
+
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
