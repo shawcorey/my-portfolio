338d5c46733260e33579558e6f3e0a2665a025ea
Corey L Shaw <shaw.corey@gmail.com>
2024-09-29 20:18:12 -0500

    added project links and additonal changes to the home page

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
 
                 {/* Define Routes */}
                 <Routes>
-                    <Route path="/" element={<About />} />
+                    
                     <Route path="/about" element={<About />} />
                     <Route path="/projects" element={<Projects />} />
                     <Route path="/blog" element={<Blog />} />
---
+++ src/components/projects.component.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './projects.component.css'; // Optional: Create a CSS file for styling.
+
+const Projects = () => {
+    return (
+        <section className="projects-section">
+            <div className="container">
+                <h2>About Me</h2>
+                <p>
+                    Hello! I'm Corey, a passionate Full Stack Web Developer with experience in building web applications using modern technologies like Java, JavaScript, React.js, Python, Node.js, and more.
+                </p>
+                <p>
+                    I have 3 years of professional experience working on projects that span across various domains. I specialize in frontend and backend development, and I'm committed to writing clean, efficient, and scalable code.
+                </p>
+                <p>
+                    Outside of coding, I enjoy fitness, being a Father, making Art, traveling and a long list of other activities. My goal is to continue growing as a developer, learn new skills, and contribute to impactful projects.
+                </p>
+                <p>
+                    Feel free to explore my projects and blog posts to learn more about my work.
+                </p>
+            </div>
+        </section>
+    );
+};
+
+export default Projects;
