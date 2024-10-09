a342ec7ffff815f23c4225de28135a6d5938f8f4
Corey L Shaw <shaw.corey@gmail.com>
2024-10-09 17:33:21 -0500

    Created Profile Pic
    Added pic to code

--- src/components/about.component.js
+++ src/components/about.component.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './about.component.css'; // Optional: Create a CSS file for styling.
-
+import profilePic from './assets/codeupprofilepic.jpg'; // Adjust path as needed
 const About = () => {
     return (
         <section className="about-section">
             <div className="container">
+            <img 
+              src={process.env.PUBLIC_URL + "/codeupprofilepic.jpg"} 
+              alt="Profile" 
+              className="about-profile-pic" 
+        />
+
+
                 <h2>About Me</h2>
                 <p>
                     Hello! I'm Corey, a passionate Full Stack Web Developer with experience in building web applications using modern technologies like Java, JavaScript, React.js, Python, Node.js, and more.
