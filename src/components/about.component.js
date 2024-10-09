d3fd6ddef0661560189161a24d594a9f65760404
Corey L Shaw <shaw.corey@gmail.com>
2024-10-09 18:23:50 -0500

    Update about.component.js
    Profile pic is properly loading now.

--- src/components/about.component.js
+++ src/components/about.component.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './about.component.css'; // Optional: Create a CSS file for styling.
-
+import profilePic from '../assets/codeupprofilepic.jpg'; // Import the image
 const About = () => {
-    console.log(process.env.PUBLIC_URL);
+    console.log("What Up Doe");
     return (
         <section className="about-section">
             <div className="container">
             <img 
-              src={process.env.PUBLIC_URL + "/codeupprofilepic.jpg"} 
+              src={profilePic} 
               alt="Profile" 
               className="about-profile-pic" 
         />
