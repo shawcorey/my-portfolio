630bd41133dc5d190fbb875f0576980497868105
Corey L Shaw <shaw.corey@gmail.com>
2024-10-10 21:04:44 -0500

    Create home.route.js
    Home route and css process started

---
+++ src/routes/home.route.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Home from './routes/Home';
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome home</h1>
+            <p>This is home for now</p>
+        </div>
+    );
+};
+
+
+export default Home;
