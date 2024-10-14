36205315a753d4a04c12271c2eadc3f408ed978d
Corey L Shaw <shaw.corey@gmail.com>
2024-10-13 20:56:10 -0500

    Update home.component.js
    Updated appropriate file to have video displayed.

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './home.component.css'; // Optional: Create a CSS file for styling if needed
-
+import videoFile from '../assets/Landing-Page.mp4';
 const Home = () => {
   return (
     <div className="home-container">
       <div className="video-container">
         <video className="home-video" autoPlay muted loop>
-          <source src="/assets/Landing-Page.mp4" type="video/mp4" />
+          <source src={videoFile} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
